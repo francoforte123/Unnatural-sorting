352e1c54c83afcfcf82d52d3aefe8f2bedcb305c
Emmanuele La Duca <laducaemanuele20@gmail.com>
2023-07-04 14:15:29 +0200

    Aggiungere i file di progetto.

---
+++ Unnatural sorting/Program.cs
@@ -0,0 +1,20 @@
+﻿List<int> listOfNumbersRandom = new();
+Random numbersRandom= new Random();
+
+for(int i = 0; i < 1001; i++)
+{
+    listOfNumbersRandom.Add(numbersRandom.Next(0, 1001));
+}
+
+Console.WriteLine("sto stampando i numeri random presenti nella lista:");
+foreach (int numbers in listOfNumbersRandom)
+{
+    Console.WriteLine(numbers);
+}
+
+List<int> listOrdered= listOfNumbersRandom.OrderBy(x => x.ToString().Length).ToList();
+
+listOrdered.ForEach(Console.WriteLine);
+
+
+
